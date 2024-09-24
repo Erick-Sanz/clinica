5411263859f573165420aa2e7450845c8125e6d1
Erick-Sanz <usuario4910@gmail.com>
2024-09-24 13:37:46 -0600

    Fixed middleware bug causing conflicts with Vercel

--- src/middlewares/global_middleware.ts
+++ src/middlewares/global_middleware.ts
@@ -1,35 +1,28 @@
 import {
-  BadRequestException,
+  HttpException,
+  HttpStatus,
   Injectable,
   NestMiddleware,
 } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import * as multer from 'multer';
 import { GlobalLogger } from 'src/loggers/logger';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   constructor(private globalLogger: GlobalLogger) {}
-  private upload = multer().any();
 
   use(req: Request, res: Response, next: NextFunction) {
-    this.upload(req, res, (err) => {
-      if (err) {
-        this.globalLogger.errorLog({
-          message: `Error processing form data: ${err.message}`,
-        });
-        throw new BadRequestException('Error processing form data');
+    if (req.method === 'POST' || req.method === 'PATCH') {
+      if (!req.body || Object.keys(req.body).length === 0) {
+        throw new HttpException(
+          'El body de la solicitud no puede estar vacío',
+          HttpStatus.BAD_REQUEST,
+        );
       }
-
-      if (req.method === 'POST' || req.method === 'PATCH') {
-        if (!req.body || Object.keys(req.body).length === 0) {
-          this.globalLogger.errorLog({
-            message: `POST request came with an empty body`,
-          });
-          throw new BadRequestException('The request body cannot be empty');
-        }
-      }
-      next();
-    });
+      this.globalLogger.errorLog({
+        message: `${req.method} request body: ${JSON.stringify(req.body)}`,
+      });
+    }
+    next();
   }
 }
