7bcff30d54b88cc1bc039ddea0c77a716f3fa881
Erick-Sanz <usuario4910@gmail.com>
2024-09-26 18:00:00 -0600

    validation regex

--- src/doctors/dto/create-doctor.dto.ts
+++ src/doctors/dto/create-doctor.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 export class CreateDoctorDto {
   @IsNotEmpty()
   @IsString()
-  @Matches(/^[a-zA-Z0-9]+$/, {
+  @Matches(/^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]+$/, {
     message: 'The name must not contain special characters',
   })
   @Length(3, 40)
